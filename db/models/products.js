8b80db278e94c411c600421210f36f129730ccba
Neel <krishlal925@gmail.com>
2020-03-25 22:19:27 -0700

    product pictures added

--- db/models/products.js
+++ db/models/products.js
@@ -4,9 +4,9 @@ const products = {
   read: async () => {
     return (await client.query("SELECT * from products")).rows;
   },
-  create: async ({ name, description, price, avail }) => {
-    const SQL = `INSERT INTO products(name, description, price, avail) values($1, $2, $3, $4) returning *`;
-    return (await client.query(SQL, [name, description, price, avail])).rows[0];
+  create: async ({ name, description, price, avail, image }) => {
+    const SQL = `INSERT INTO products(name, description, price, avail, image) values($1, $2, $3, $4, $5) returning *`;
+    return (await client.query(SQL, [name, description, price, avail, image])).rows[0];
   },
 };
 
